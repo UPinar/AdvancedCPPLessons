55a986d69851ce0ac72de64bca2ef6cd54c52fef
UPinar <uygrpinar@gmail.com>
2024-10-17 19:09:02 +0300

    lesson_12_first

---
+++ lambda_expressions/lambda_expressions.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+/*
+                      ======================
+                      | lambda expressions |
+                      ======================
+*/
+
+/*
+  - bir lambda ifadesi(lambda expression), derleyicinin oluşturduğu
+    bir sınıf türünden bir geçici nesnedir(temporary object).,
+    PRValue expression
+  - lambda ifadesi karşılığı derleyicinin oluşturduğu sınıf türüne 
+    closure type denir.
+  - lamda ifadesi karşılığı derleyicinin oluşturduğu geçici nesneye
+    closure object denir.
+*/
+
+// Lesson_12 - 01:02:00
